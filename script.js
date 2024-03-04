871dcb2de369fa2ca3d132c85cefd649fd83413d
muchubatactics <byooxmckeith@gmail.com>
2024-03-04 22:53:35 +0300

    special technique

--- script.js
+++ script.js
@@ -218,3 +218,19 @@ DomDisplay.prototype.scrollPlayerIntoView = function(state) {
 let simpleLevel = new Level(simpleLevelPlan);
 let display = new DomDisplay(document.body, simpleLevel);
 display.syncState(State.start(simpleLevel));
+
+Level.prototype.touches = function(pos, size, type) {
+  let xStart = Math.floor(pos.x);
+  let xEnd = Math.ceil(pos.x + size.x);
+  let yStart = Math.floor(pos.y);
+  let yEnd = Math.ceil(pos.y + size.y);
+
+  for (let y = yStart; y < yEnd; y++) {
+    for (let x = xStart; x < xEnd; x++) {
+      let isOutside = x < 0 || x >= this.width || y < 0 || y >= this.height;
+      let here = isOutside ? "wall" : this.rows[y][x];
+      if (here == type) return true;
+    }
+  }
+  return false;
+};
